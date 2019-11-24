187d780264fd5547b533c6279fab22ec09de429d
Aleksandr <boss@myroom.ru>
2019-11-24 23:37:09 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,7 @@ if ! [ -d ~/.homebridge ]; then
 sudo mkdir ~/.homebridge && sudo cp ./config.json ~/.homebridge
 fi
 
-sudo npm install -g --unsafe-perm homebridge-config-ui-x
-#sudo npm i -g --unsafe-perm homebridge homebridge-config-ui-x
+sudo npm i -g --unsafe-perm homebridge homebridge-config-ui-x
 
 sudo bash -c "cat > /etc/systemd/system/homebridge.service" << EOL
 [Unit]
@@ -27,6 +26,7 @@ AmbientCapabilities=CAP_NET_RAW
 [Install]
 WantedBy=multi-user.target
 EOL
+
 
 #sudo bash -c "cat > /etc/systemd/system/homebridge-config-ui-x.service" << EOL
 #[Unit]
