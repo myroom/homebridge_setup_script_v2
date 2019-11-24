8ccde34bd5598e7aed141a6b1818a509d46f364c
Aleksandr <boss@myroom.ru>
2019-11-24 21:06:11 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -44,7 +44,9 @@ WantedBy=multi-user.target
 EOL
 
 sudo systemctl daemon-reload
+
 sudo systemctl enable homebridge
 sudo systemctl start homebridge
+
 sudo systemctl enable homebridge-config-ui-x
 sudo systemctl start homebridge-config-ui-x
